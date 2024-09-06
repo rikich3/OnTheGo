9938affbf1ac201226240e4e5ffea10ebe69c228
rikich3 <rchambillap@unsa.edu.pe>
2024-09-06 14:47:06 -0500

    on set_uwt.sh: adding the effect to sustitute the variable if its set already

--- set_uwt.sh
+++ set_uwt.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-var=$1
+var="${1}:"
 
-line=$(grep -n "$var" env.local | )
-
-sed "d$line"
+line=$(grep -n "$var" env.local | cut -d: -f1)
+if [[ $line != "" ]]; then
+  sed -i "${line}d" env.local
+fi
+echo "Ingrese el valor para la variable ${var}"
 read setvar
 echo "${var}${setvar}" >> env.local
